a0bc04c2ce53645d09c0b6c4fded6ed7ada46222
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-11-16 13:20:27 +0000

    Ensure we add the plugins, then init the DB

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-. "${CKAN_HOME}/bin/activate"
+
+if [ $# -eq 0 ] ; then
+  . "${CKAN_HOME}/bin/activate"
+fi
 
 set -eu
 
@@ -8,16 +11,25 @@ if [ $# -eq 0 ] ; then
   "${ENTRYPOINT_SCRIPT_HOME}/configure.sh"
 
   shopt -s nullglob
+  for SUBDIR in ${CKAN_PLUGINS}/* ; do
+    if [[ -d "${SUBDIR}" ]]; then
+      cd ${SUBDIR}
+      python "setup.py" develop
+      cd -
+    fi
+  done
+  shopt -u nullglob
 
-  for USER_SCRIPT in $USER_SCRIPT_DIR/* ; do
+  "${ENTRYPOINT_SCRIPT_HOME}/initdb.sh"
+
+  shopt -s nullglob
+  for USER_SCRIPT in ${USER_SCRIPT_DIR}/* ; do
     if [[ -x "${USER_SCRIPT}" ]]; then
       "${USER_SCRIPT}"
     fi
   done
-
   shopt -u nullglob
 
-  "${ENTRYPOINT_SCRIPT_HOME}/initdb.sh"
   exec httpd -D FOREGROUND
 else
   exec $@
