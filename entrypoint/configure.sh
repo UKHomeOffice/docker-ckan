e18daf7d1c088b8d8d230e359e375d944d07de37
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-08-09 12:41:14 +0100

    corrected spelling mistake

--- entrypoint/configure.sh
+++ entrypoint/configure.sh
@@ -20,7 +20,7 @@ write_config () {
   paster make-config ckan "$CONFIG"
 
   paster --plugin=ckan config-tool "$CONFIG" -e \
-      "sqlalcqhemy.url = postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:5432/ckan" \
+      "sqlalchemy.url = postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:5432/ckan" \
       "solr_url = ${CKAN_SOLR_URL}" \
       "solr_name = ${SOLR_NAME}" \
       "solr_password = ${SOLR_PASSWORD}" \
